7695d5f77df8441d8d362c4fd410ae28c4c2c5b3
Jonathan Bull <u1411318@live.warwick.ac.uk>
2016-12-04 14:30:54 +0000

    added kieran's code

--- CS346.java
+++ CS346.java
@@ -21,6 +21,7 @@ public class CS346 {
 	private static int Qr;
 	private static int n;
 	private Server[] servers;
+	private Thread[] serverThreads;
     
     public static void usage() {
         System.out.println("Usage:");
@@ -71,7 +72,7 @@ public class CS346 {
 	//each server is assigned a unique id
 	//a server has a log manager
 	//a server has a lock manager
-	private class Server {
+	private class Server implements Runnable{
 		private int id;
 		private int port;
 		private int dataItem;
@@ -294,8 +295,9 @@ public class CS346 {
 		
 		//spawn servers
 		cs346.spawnServers();
+		cs346.startServers();
 		
-		//
+		//spawn and start clients
 		cs346.spawnClients();
     }
     
@@ -310,6 +312,13 @@ public class CS346 {
 			servers[i] = new Server(serverID, serverPort);
 		}
 	}
+	
+	public void startServers() {
+		for (int i=0; i<n; i++){
+			serverThreads[i] = new Thread(servers[i]);
+			serverThreads[i].start();
+		}	
+	}
 		
 	public void spawnClients() {
 		System.out.println("<CS346>: spawning 2 clients.");
@@ -321,109 +330,3 @@ public class CS346 {
 		thread2.start();
 	}
 }
-    // private static void serverMain(String[] args) throws IOException {
-	// if (args.length != 4) {
-            // usage();
-        // }
-        
-        // int serverListenPort = Integer.parseInt(args[1]); // The port that this server should listen on to accept connections from other servers
-        // int clientListenPort = Integer.parseInt(args[2]); // The port that this server should listen on to accept connections from clients
-        // InetSocketAddress[] otherServerAddresses = Server.parseAddresses(args[3]); // The addresses of the other servers
-        // String databasePath = "test"; // The path to the database file
-        
-        // Server server = new Server(databasePath);
-        
-     
-	// try {
-	
-	    // if (serverListenPort == 30001) {
-		// System.out.println("<server> Accepting client on " + clientListenPort);
-			
-		// server.connectServers(otherServerAddresses);
-		//server.acceptClients(clientListenPort);	
-	    // }
-	    // else {
-		// System.out.println("<server> Accepting coordinator on " + serverListenPort);
-	
-		// ServerSocket serverSocket = new ServerSocket(serverListenPort);
-		
-		// try {
-		    // Socket socket = serverSocket.accept();
-		    // System.out.println("<server> Successfully connected to coordinator");
-		// } finally {
-		      // serverSocket.close();
-		// }
-	    // }
-			
-	    // while (true){
-			
-	    // }
-	// } catch (IOException e) {
-	    // System.err.println(e);
-	    // e.printStackTrace(System.err);
-	// } finally {
-	     //server.close();
-	      
-	// }
-// }
-// }
-
-/* 	 private class Server {
-
-		// private String databasePath;
-
-		public Server(String databasePath) throws IOException {
-		  this.databasePath = databasePath;
-		}
-
-		public void acceptServers(int port) throws IOException {
-		
-		System.out.println("Accepting servers on port: " + port);
-		  
-		ServerSocket serverSocket = new ServerSocket(port); 
-			Socket socket = serverSocket.accept();
-		
-			System.out.println("Successfully accepted servers on port: " + port);        	
-		
-		}
-		
-		public void connectServers(InetSocketAddress[] servers) throws IOException {
-		
-		System.out.println("Attempting to connect to other servers");
-		
-		Socket socket = new Socket(); // Socket to connect to first server
-		Socket socket2 = new Socket(); // Socket to connect to second server
-			  
-		socket.connect(servers[0]); // Connect to first server 
-		System.out.println("Successfully connected to server: " + servers[0]);
-		socket2.connect(servers[1]); // Connect to second server
-		System.out.println("Successfully connected to server: " + servers[1]);
-
-		/*InputStream is = socket.getInputStream();	// Create input/output stream 
-		OutputStream os = socket.getOutputStream();	// for first server
-		InputStream is2 = socket2.getInputStream();	// Create input/output stream
-		OutputStream os2 = socket2.getOutputStream();	// for second server
-		iStreams = new InputStream[servers.length];
-		oStreams = new OutputStream[servers.length];
-
-		iStreams[0] = is;
-		iStreams[1] = is2;	// Store the input/output streams
-		oStreams[0] = os;	// in their resoective arrays
-		oStreams[1] = os2;*/
-		
-		// }
-		
-		// public InetSocketAddress[] parseAddresses(String input) {
-			// if (input.equals("NULL")) {
-				// return new InetSocketAddress[0];
-			// }
-			// String[] rawAddresses = input.split(",");
-			// InetSocketAddress[] addresses = new InetSocketAddress[rawAddresses.length];
-			// for (int i = 0; i != rawAddresses.length; ++i) {
-				// String[] addrParts = rawAddresses[i].split(":");
-				// addresses[i] = new InetSocketAddress(addrParts[0], Integer.parseInt(addrParts[1]));
-			// }
-			// return addresses;
-		// }
-
-	// } */
